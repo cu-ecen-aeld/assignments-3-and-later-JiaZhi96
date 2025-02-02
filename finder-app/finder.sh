8cfe9f43836ba9a22a5711fb028bd2813d8ceb87
Lim Jia Zhi <34474831+JiaZhi96@users.noreply.github.com>
2025-02-02 19:08:20 +0800

    Fixed .c test. Added finder and write script. full-test.sh passed

---
+++ finder-app/finder.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+
+if [ $# != 2 ]
+then
+    echo "2 Arguments needed [filesdir] [searchstr]"
+    exit 1
+fi
+
+FILES_DIR=$1
+SEARCH_STR=$2
+
+if [ ! -d "${FILES_DIR}" ]
+then
+    echo "Directory ${FILES_DIR} do not exist"
+    exit 1
+fi
+
+NUM_OF_OCCURENCE=$(grep -o ${SEARCH_STR} ${FILES_DIR}/* | wc -l)
+NUM_OF_FILES=$(grep -l ${SEARCH_STR} ${FILES_DIR}/* | wc -l)
+
+echo "The number of files are ${NUM_OF_FILES} and the number of matching lines are ${NUM_OF_OCCURENCE}"
+
+exit 0
---
+++ finder-app/writer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ $# != 2 ]
+then
+    echo "2 Arguments needed [writefile] [writestr]"
+    exit 1
+fi
+
+WRITE_FILE=$1
+WRITE_STR=$2
+WRITE_DIR=$(dirname ${WRITE_FILE})
+
+if [ ! -d ${WRITE_DIR} ]
+then
+    mkdir -p ${WRITE_DIR}
+fi
+
+if [ ! -e ${WRITE_FILE} ]
+then
+    touch ${WRITE_FILE}
+fi
+
+echo ${WRITE_STR} > ${WRITE_FILE}
+
+exit 0
