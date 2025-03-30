d3e05ef9b3920135a9a07e0393c752a55b772f64
Lim Jia Zhi <34474831+JiaZhi96@users.noreply.github.com>
2025-03-30 17:14:33 +0800

    threading unit test passed

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -14,6 +14,49 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data *param = (struct thread_data *)thread_param;
+    int rc = 0;
+
+    if(param->wait_before_obtain_ms > 0)
+    {
+        DEBUG_LOG("Wait %dms", param->wait_before_obtain_ms);
+        rc = usleep(param->wait_before_obtain_ms * 1000);
+        if(rc != 0)
+        {
+            ERROR_LOG("Early wake sleep before obtain mutex, time left: %dms", rc);
+            return thread_param;
+        }
+    }
+
+    DEBUG_LOG("Lock mutex");
+    rc = pthread_mutex_lock(param->mutex);
+    if(rc != 0)
+    {
+        ERROR_LOG("Failed to lock mutex, rc:%d", rc);
+        return thread_param;
+    }
+
+    if(param->wait_before_release_ms > 0)
+    {
+        DEBUG_LOG("Wait %dms", param->wait_before_release_ms);
+        rc = usleep(param->wait_before_release_ms * 1000);
+        if(rc != 0)
+        {
+            ERROR_LOG("Early wake sleep before releasing mutex, time left: %dms", rc);
+            // No return because we must release the mutex
+        }
+    }
+
+    DEBUG_LOG("Unlock mutex");
+    rc = pthread_mutex_unlock(param->mutex);
+    if(rc != 0)
+    {
+        ERROR_LOG("Failed to unlock mutex, rc:%d", rc);
+        return thread_param;
+    }
+
+    param->thread_complete_success = true;
+    
     return thread_param;
 }
 
@@ -28,6 +71,25 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    struct thread_data* thread_param = malloc(sizeof(struct thread_data));
+    if(NULL == thread_param)
+    {
+        ERROR_LOG("Failed to malloc for thread_data");
+        return false;
+    }
+    thread_param->mutex = mutex;
+    thread_param->wait_before_obtain_ms = wait_to_obtain_ms;
+    thread_param->wait_before_release_ms = wait_to_release_ms;
+    thread_param->thread_complete_success = false;
+
+    int rc;
+    rc = pthread_create(thread, NULL, threadfunc, thread_param);
+    if(rc != 0)
+    {
+        ERROR_LOG("Failed to created pthread, rc:%d", rc);
+        return false;
+    }
+
+    return true;
 }
 
--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -14,7 +14,9 @@ struct thread_data{
      * between the start_thread_obtaining_mutex function and
      * your thread implementation.
      */
-
+    pthread_mutex_t *mutex;
+    unsigned int wait_before_obtain_ms;
+    unsigned int wait_before_release_ms;
     /**
      * Set to true if the thread completed with success, false
      * if an error occurred.
